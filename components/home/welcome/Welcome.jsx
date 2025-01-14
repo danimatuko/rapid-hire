c1baa7073571596dfa9f4014b34eee7f80de0b0a
Dani Matuko <danimatuko@outlook.com>
2025-01-14 18:14:30 +0200

    added job types flatlist

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,20 +1,25 @@
-import React from "react";
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from "react-native";
+import { useRouter } from "expo-router";
+import { icons, SIZES } from "../../../constants";
 
 import styles from "./welcome.style";
-import { useRouter } from "expo-router";
-import { icons } from "@/constants";
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-time");
 
   return (
     <View>
+      {/* Greeting Section */}
       <View style={styles.container}>
         <Text style={styles.userName}>Hello Dani</Text>
         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
       </View>
 
+      {/* Search Section */}
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
           <TextInput
@@ -32,6 +37,27 @@ const Welcome = () => {
             style={styles.searchBtnImage}
           />
         </TouchableOpacity>
+      </View>
+
+      {/* Job Type Tabs */}
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
       </View>
     </View>
   );
