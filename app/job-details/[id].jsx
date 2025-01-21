82fd3f571df8e46560ce83697e493ece40bc8f23
Dani Matuko <danimatuko@outlook.com>
2025-01-21 21:37:27 +0200

    Refactor JobDetails component to use early returns for cleaner code structure. Added RefreshControl for pull-to-refresh functionality.

--- app/job-details/[id].jsx
+++ app/job-details/[id].jsx
@@ -1,24 +1,82 @@
-import { useLocalSearchParams, useRouter, Stack } from 'expo-router'
-import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
-import ScreenHeaderBtn from '@/components/common/header/ScreenHeaderBtn' // Ensure correct import
+import { useLocalSearchParams, Stack } from 'expo-router'
+import React, { useState } from 'react'
+import {
+  SafeAreaView,
+  Text,
+  View,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from 'react-native'
+import ScreenHeaderBtn from '@/components/common/header/ScreenHeaderBtn'
 import { COLORS, icons } from '@/constants'
+import { Company, JobAbout, JobFooter } from '@/components'
+import useFetch from '../../hooks/useFetch'
 
 const JobDetails = () => {
   const params = useLocalSearchParams()
+  const [refreshing, setRefreshing] = useState(false)
+
+  const { isLoading, data, error } = useFetch('job-details', {
+    job_id: params.id,
+  })
+
+  const onRefresh = () => {}
+
+  if (isLoading) {
+    return <ActivityIndicator size='large' color={COLORS.primary} />
+  }
+
+  if (error) {
+    return (
+      <Text style={{ color: COLORS.error, textAlign: 'center' }}>
+        Something went wrong. Please try again later.
+      </Text>
+    )
+  }
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
         options={{
           headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
           headerLeft: () => (
-            <ScreenHeaderBtn iconUrl={icons.left} onPress={() => {}} /> // Add a basic onPress handler for now
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension='60%'
+              handlePress={() => {}}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.share}
+              dimension='60%'
+              handlePress={() => {}}
+            />
           ),
           headerTitle: '',
         }}
       />
-      <Text>Job Details Screen</Text>
+
+      <ScrollView
+        contentContainerStyle={{ padding: 20 }}
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+            colors={[COLORS.primary]}
+          />
+        }
+      >
+        <View>
+          <Company company={data?.company} />
+          <JobAbout description={data?.description} />
+          <JobFooter />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
